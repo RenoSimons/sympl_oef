329bae877430aaaec8d407948a87bdc5ad7c0dc4
reno simons <simons.reno@skynet.be>
2021-09-10 11:54:47 +0200

    Added seeder for data to work with

--- oefening_sympl/database/seeders/DatabaseSeeder.php
+++ oefening_sympl/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Make 5 users
+        for ($i=1; $i < 5; $i++) {
+            DB::table('users')->insert([
+                'name' => "User" . $i,
+                'email' => 'user@user' . $i .'com',
+            ]);
+        }
+        
+        // Make 5 projects
+        for ($i=1; $i < 5; $i++) {
+            DB::table('projects')->insert([
+                'project_name' => "Project" . $i,
+            ]);
+        }
+
+        // Make 5 roles
+        for ($i=1; $i < 5; $i++) {
+            DB::table('roles')->insert([
+                'role_name' => "Role" . $i,
+            ]);
+        }
     }
 }
